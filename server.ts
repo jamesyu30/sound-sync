ed0d643c9312c95345fd0cc961ee1993e770f0f3
James Yu <jyu30@stuy.edu>
2025-09-27 23:05:14 -0400

    fixes

--- server.ts
+++ server.ts
@@ -477,7 +477,7 @@ app.use((req, res, next) => {
 });
 
 // SPA fallback only for browser navigations (GET)
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(process.cwd(), "dist", "client", "index.html"));
 });
 
