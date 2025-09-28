47bff453b9c14bd37c2bae671acfb06835114343
James Yu <jyu30@stuy.edu>
2025-09-27 22:16:40 -0400

    api fixes

--- server.ts
+++ server.ts
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  //console.log(`${new Date().toISOString()} ${req.method} ${req.originalUrl} Origin:${req.headers.origin || "-"}`);
+  console.log(`${new Date().toISOString()} ${req.method} ${req.originalUrl} Origin:${req.headers.origin || "-"}`);
   next();
 });
 
